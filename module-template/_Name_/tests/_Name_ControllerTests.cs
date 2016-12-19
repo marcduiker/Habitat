014f2bbbffff01a6a6a025a6b37e54f3bec8e96d
Marc Duiker <marcduiker@gmail.com>
2016-12-19 23:33:13 +0100

    Added module template files

---
+++ module-template/_Name_/tests/_Name_ControllerTests.cs
@@ -0,0 +1,37 @@
+﻿namespace _NamespacePrefix_._ModuleType_._Name_.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Web.Mvc;
+  using Controllers;
+  using FakeDb;
+  using FluentAssertions;
+  using Repositories;
+  using Xunit;
+  using Foundation.Testing.Attributes;
+  using Models;
+  using NSubstitute;
+  using Pipelines;
+
+
+  public class _Name_ControllerTests
+  {
+    [Theory]
+    [AutoDbData]
+    public void DefaultConstructor_ShouldNotThrow()
+    {
+      Action act = () => new _Name_Controller();
+      act.ShouldNotThrow();
+    }
+
+    [Theory]
+    [AutoDbData]
+    public void Constructor_ShouldNotThrow(I_Name_Repository _Name_Repository)
+    {
+      Action act = () => new _Name_Controller(_Name_Repository);
+      act.ShouldNotThrow();
+    }
+
+    // TODO: Implement unit tests for the _Name_Controller
+  }
+}
